9aa67a3443196c38c4e70cea93963a1959daa451
Shang Nan <sinoon1218@gmail.com>
2014-12-15 17:18:27 +0800

    增加js文件整合

--- fis-conf.js
+++ fis-conf.js
@@ -1,2 +1,15 @@
 fis.config.set('modules.parser.less', 'less');
 fis.config.set('roadmap.ext.less', 'css');
+fis.config.set('modules.postpackager', 'simple');
+
+fis.config.set('pack', {
+    'all.js': [
+        '/zepto.js',
+        '/eventproxy.js',
+        '/touch.js',
+        '/main.js',
+        '/touchfixed.js',
+        '/stb.js'
+    ]
+});
+
